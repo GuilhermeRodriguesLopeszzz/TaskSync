81d5883ae1dfbe1b6617bdd99ce47dddf7ccf3d1
GUILHERME RODRIGUES LOPES <guilherme.lopes3@alunos.sc.senac.br>
2026-08-21 21:43:19 -0300

    Aula 21/08/2026

---
+++ src/main/java/com/example/tasksync/controllers/AuthController.java
@@ -0,0 +1,35 @@
+package com.example.tasksync.controllers;
+
+import com.example.tasksync.DTOs.LoginRequest;
+import com.example.tasksync.services.TokenService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.HttpURLConnection;
+
+@RestController
+@RequestMapping("/auth")
+@Tag(description = "Controller de autenticação", name="Autenticação")
+public class AuthController {
+
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/login")
+    @Operation(description = "Método de login", summary = "Autenticação de usuarios")
+    public ResponseEntity<?> login(@RequestParam LoginRequest loginRequest){
+
+        if(loginRequest.email().equals("string")&& loginRequest.senha().equals("string")){
+            //Gera Token
+            var token = tokenService.gerarToken(loginRequest.email());
+
+            return ResponseEntity.ok(token);
+        }
+        return ResponseEntity.status(HttpURLConnection.HTTP_UNAUTHORIZED).build();
+    }
+
+}
--- src/main/java/com/example/tasksync/controllers/UsuarioController.java
+++ src/main/java/com/example/tasksync/controllers/UsuarioController.java
@@ -2,6 +2,8 @@ package com.example.tasksync.controllers;
 
 import com.example.tasksync.entities.Usuario;
 import com.example.tasksync.repository.UsuarioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +13,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
+@Tag(name="Usuarios", description = "Grupo de APIs responsavel por controlar a estrutura de crição e consulta de usuario do sistema!")
 public class UsuarioController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
 
     @GetMapping
+    @Operation (summary = "Metodo de consulta de lista de usuario", description = "Metodo responsavel em efetuar a consulta de todos os usuarios sem filtro")
     public ResponseEntity<?> ListarTodos(){
 
         return ResponseEntity.ok(usuarioRepository.findAll());
@@ -24,6 +28,7 @@ public class UsuarioController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation (summary = "Metodo de criação de usuario", description = "Metodo responsavel em efetuar a criação de novos usuarios")
     public ResponseEntity<Usuario> criar(@RequestBody Usuario usuario){
 
         var usuarioBanco = usuarioRepository.save(usuario);
